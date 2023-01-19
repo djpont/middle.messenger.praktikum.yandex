c3aa454adaf4c67e73b630b51c97d070b2e8e624
djpont <alexandervotyakov@gmail.com>
2023-01-19 12:22:41 +0400

    Дал инпутам конкретные атрибуты name.

--- src/pages/sign-up/index.js
+++ src/pages/sign-up/index.js
@@ -12,73 +12,74 @@ export default (rootElement) => {
 		'sign-up',
 		'sign-up',
 		'WinChat 98 - Регистрация',// 'Регистрация в Мессенджере',
-		{close:true}
+		{close: true}
 	);
 	rootElement.append(page.document());
 	
 	// Генерируем контент по шаблону
-	const document=generateDom(tpl({
-		nameLine:inputWithLabel({
-			id:'name',
-			type:'text',
-			name:'name',
-			label:'Имя:',
-			isStacked:true
+	const document = generateDom(tpl({
+		infoLines: [
+			inputWithLabel({
+				id: 'first_name',
+				type: 'text',
+				name: 'first_name',
+				label: 'Имя:',
+				isStacked: true
+			}),
+			inputWithLabel({
+				id: 'second_name',
+				type: 'text',
+				name: 'second_name',
+				label: 'Фамилия:',
+				isStacked: true
+			}),
+			inputWithLabel({
+				id: 'email',
+				type: 'email',
+				name: 'email',
+				label: 'Адрес электронной почты:',
+				isStacked: true
+			}),
+			inputWithLabel({
+				id: 'phone',
+				type: 'text',
+				name: 'phone',
+				label: 'Телефон:',
+				isStacked: true
+			}),
+			inputWithLabel({
+				id: 'login',
+				type: 'text',
+				name: 'login',
+				label: 'Логин:',
+				isStacked: true
+			}),
+			inputWithLabel({
+				id: 'password',
+				type: 'password',
+				name: 'password',
+				label: 'Пароль:',
+				isStacked: true
+			}),
+			inputWithLabel({
+				id: 'password2',
+				type: 'password',
+				name: 'password2',
+				label: 'Пароль ещё раз:',
+				isStacked: true
+			})
+		],
+		buttonSubmit: button({
+			id: 'submit',
+			name: 'submit',
+			type: 'submit',
+			value: 'Регистрация'
 		}),
-		surnameLine:inputWithLabel({
-			id:'surname',
-			type:'text',
-			name:'surname',
-			label:'Фамилия:',
-			isStacked:true
-		}),
-		emailLine:inputWithLabel({
-			id:'email',
-			type:'email',
-			name:'email',
-			label:'Адрес электронной почты:',
-			isStacked:true
-		}),
-		phoneLine:inputWithLabel({
-			id:'phone',
-			type:'text',
-			name:'phone',
-			label:'Телефон:',
-			isStacked:true
-		}),
-		loginLine:inputWithLabel({
-			id:'login',
-			type:'text',
-			name:'login',
-			label:'Логин:',
-			isStacked:true
-		}),
-		passwordLine1:inputWithLabel({
-			id:'password1',
-			type:'password',
-			name:'password',
-			label:'Пароль:',
-			isStacked:true
-		}),
-		passwordLine2:inputWithLabel({
-			id:'password2',
-			type:'password',
-			name:'password2',
-			label:'Пароль ещё раз:',
-			isStacked:true
-		}),
-		
-		buttonSubmit:button({
-			id:'submit',
-			name:'submit',
-			type:'submit',
-			value:'Регистрация'
-		}),
-		buttonCancel:button({
-			id:'cancel',
-			name:'cancel',
-			type:'button',
-			value:'Отмена'
+		buttonCancel: button({
+			id: 'cancel',
+			name: 'cancel',
+			type: 'button',
+			value: 'Отмена'
 		})
 	}));
 	page.content().append(document);
